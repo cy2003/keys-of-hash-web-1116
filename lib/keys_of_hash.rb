11ca24f83cf00fe5150c087349504e4de6115501
Cathy Yee <catyyee@hotmail.com>
2016-11-18 16:27:20 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)           #putting '*' before object invokes the splat operator. Let's you pass multiple arguments in an array
+    array = []
+    self.each do |key, value|       #self refers to the object(i.e. the hash object - 'animals')
+      arguments.each do |argument|
+        if value == argument
+          array << key
+        end
+      end
+    end
+    array
   end
 end
